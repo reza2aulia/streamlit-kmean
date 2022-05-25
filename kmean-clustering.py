66e7786deec77af0b00d3480ab818171ed7405b9
reza2aulia <36034153+reza2aulia@users.noreply.github.com>
2022-05-25 16:52:38 +0700

    Add files via upload

--- kmean-clustering.py
+++ kmean-clustering.py
@@ -1,90 +1,57 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[18]:
-
-
 import warnings
-import time
-import webbrowser
+from itertools import combinations
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import time
+import random
 import streamlit as st
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
-warnings.filterwarnings('ignore')
-
-
-# In[2]:
+warnings.filterwarnings("ignore")
 
 
 df = pd.read_csv('Country-data.csv')
-
-# In[3]:
-
-
-df_num = df.select_dtypes(include='number')
-
-# In[19]:
-
-
-df2 = df[['health', 'gdpp']].copy()
-
 st.title("Interactive K-Means Clustering")
 
-st.write("Berikut ini tampilan dataset untuk di Analisis clustering:")
+st.write("Data Indikator Kesejahteraan Negara")
 
-st.dataframe(df2)
+st.dataframe(df)
 
+# scalling
+num_kolom = df.select_dtypes(include=np.number).columns.tolist()
+scaler = MinMaxScaler()
+df[num_kolom] = scaler.fit_transform(df[num_kolom])
 
-# In[6]:
-
-
-# In[7]:
-
-
-rscaled = RobustScaler()
-
-
-# In[8]:
-
-
-rscaled.fit(df2)
-
-
-# In[9]:
-
-
-df2_scaled = rscaled.transform(df2)
-
-
-# In[10]:
-
-
-df2_scaled = pd.DataFrame(df2_scaled)
-df2_scaled.columns = df2.columns
-
-
-# In[11]:
-
-
-df_num = df.select_dtypes(include=np.number).columns
-rscaler = RobustScaler()
-df[df_num] = rscaler.fit_transform(df[df_num])
 
 # sidebar
 with st.sidebar:
     st.title('Reza Aulia')
-    st.write("Perkenalkan Nama Saya Reza Aulia.")
-    st.write('NPM : 2108207010004')
-    st.write(
-        "Hay berikut Project GUI menggunakan Streamlit.")
-    nomor = st.slider('Pilih nomor untuk dilakukan cluster',
+
+    # select box
+
+    option1 = st.selectbox(
+        'Pilihan 1',
+        ([None] + num_kolom))
+
+    st.write('You selected:', option1)
+
+    if option1 is None:
+        st.write("Pilihan 1 harus diisi")
+
+    option2 = st.selectbox(
+        'Pilihan 2',
+        ([None] + num_kolom))
+
+    st.write('You selected:', option2)
+    if option2 is None:
+        st.write("Pilihan 2 harus diisi")
+
+    nomor = st.slider('Pilih jumlah cluster yang anda inginkan',
                       min_value=2, max_value=10)
     with st.spinner("Loading..."):
         time.sleep(5)
@@ -95,17 +62,27 @@ with st.sidebar:
     if st.button('LinkedIn'):
         webbrowser.open_new_tab(url)
 
+# cluster
+if option1 == option2:
+    st.warning('Pilihan tidak boleh sama')
+else:
+    if option1 is None:
+        st.warning('Pilihan 1 tidak boleh kosong')
+    elif option2 is None:
+        st.warning('Pilihan 2 tidak boleh kosong')
+    else:
+        st.write(
+            "Pengelompokan Negara Berdasarkan Indikator Kesejahteraan Dengan Metode Unsupervised Learning-Clustering:")
 
-# n = int(input('silahkan masukkan angka:'))
-kmeans = KMeans(n_clusters=nomor, init='k-means++')
-kmeans.fit(df2)
+        df_cluster = df[[option1, option2]].copy()
+        kmeans = KMeans(n_clusters=nomor, init='k-means++')
+        kmeans.fit(df_cluster)
 
-df2['label'] = kmeans.labels_
+        labels = kmeans.labels_
+        centroid = kmeans.cluster_centers_
 
-
-st.write("Jumlah clustering yang dipilih {} Cluster :".format(nomor))
-st.write("Berikut Visualisasi dari Model K-Mean")
-sns.scatterplot(data=df2, x='health', y='gdpp', hue='label', palette="deep")
-sns.scatterplot(x=kmeans.cluster_centers_[
-                :, 0], y=kmeans.cluster_centers_[:, 1], marker='*', s=500)
-st.pyplot()
+        sns.scatterplot(data=df_cluster, x=option1, y=option2, hue=labels)
+        sns.scatterplot(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[
+            :, 1], marker='*', s=500)
+        plt.show()
+        st.pyplot()
